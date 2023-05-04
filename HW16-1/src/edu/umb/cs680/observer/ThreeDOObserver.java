8f19d7b77cf965a93abf492c1db6f8086e962ed7
someshsb1 <someshbalanisb@gmail.com>
2023-05-04 00:27:53 -0400

    update

--- HW16-1/src/edu/umb/cs680/observer/ThreeDOObserver.java
+++ HW16-1/src/edu/umb/cs680/observer/ThreeDOObserver.java
@@ -4,6 +4,7 @@ public class ThreeDOObserver implements Observer<StockEvent> {
 
     @Override
     public void update(Observable<StockEvent> sender, StockEvent event) {
+        //Call event.getTicker() and event.getQuote() and process the 2 info
         System.out.println("ThreeDOObserver: " + event.getTicker() + " " + event.getQoute());
         
     }
